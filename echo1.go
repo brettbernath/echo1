264c1f1845e48da24e6e0077c5b986fdff64dabf
Brett Bernath <afineprogrammer@gmail.com>
2018-05-22 15:33:05 -0700

    initial commit

---
+++ echo1.go
@@ -0,0 +1,16 @@
+//Echo1 prints its command line arguments
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+
+    fmt.Println( "The name of the program is " + os.Args[0] );
+
+    for i := 1; i < len(os.Args); i++ {
+        fmt.Printf( "%d %s\n", i, os.Args[i] );
+    }
+}
